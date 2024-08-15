9e146c760c6a0ffde09a4785953586722a52d92e
molly <mollynzembi@gmail.com>
2024-08-15 17:47:38 +0300

    login

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
+//import axios from 'axios';
 
 const LoginPage = () => {
   const [email, setEmail] = useState('');
